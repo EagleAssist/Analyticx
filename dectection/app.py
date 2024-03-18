c94a1c6ada15434d23e37a54846109fe367eb437
DanBrown47 <danwand47@gmail.com>
2024-03-19 00:41:32 +0530

    Add  RabbitMQ in Dectection

--- dectection/app.py
+++ dectection/app.py
@@ -4,6 +4,12 @@ import pika  # RabbitMQ
 
 app = Flask(__name__)
 
+
+# TODO : Move this to a separate file
+def fetch_from_minio(frame_path):
+    # TODO : Fetch from Minio
+    pass
+
 @app.route('/detect', methods=['POST'])
 def detect():
     image = request.files['image']
@@ -12,14 +18,21 @@ def detect():
     return dectect_elements(image_path)
 
 
-def connect_and_listen():
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=rabbitmq_host, credentials=credentials))
-    channel = connection.channel()
-    channel.queue_declare(queue=queue_name) 
+def process_message(ch, method, properties, body):
+    print(f' [x] Received {body}')
+    # TODO : Fetch from Minio and respond to dectect elements use f_get_object
+    fetch_from_minio(body)
+    dectect_elements(body)
+    print(f' [x] Done')
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
-    channel.basic_consume(queue=queue_name, 
-                          on_message_callback=process_message, 
+def connect_and_listen():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    channel = connection.channel()
+    # channel.queue_declare(queue=queue_name) Should be declared in the ingestion module 
+
+    channel.basic_consume(queue='raw-frames', 
+                          on_message_callback=process_message, # which process to perform
                           auto_ack=True)  # Acknowledge messages automatically
 
     try:
